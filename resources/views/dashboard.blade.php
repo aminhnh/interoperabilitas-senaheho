a18c614bfb7c6b827cc7d24008ba5206aae09124
Marwahkamilaahmad <Mymarwahmad@gmail.com>
2024-05-29 10:10:57 +0700

    add frontend

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,10 +3,44 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Dashboard</title>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </head>
 <body>
 
-<h2>Dashboard</h2>
+    <div class="container text-center mt-5">
+        <h2 class="">Selamat datang, User!</h2>
+        <div class="">Apa yang ingin Anda lakukan hari ini?</div>
+
+        <div class="row mt-4 justify-content-center">
+            <div class="col-md-3">
+                <button class="card card-custom">
+                    <img src="/assets/healthicons_blood-bag.png" class="card-img-top" alt="Icon 1">
+                    <div class="card-body">
+                        <h5 class="card-title">Persediaan Darah</h5>
+                    </div>
+                </button>
+            </div>
+            <div class="col-md-3">
+                <button class="card card-custom">
+                    <img  src="/assets/ri_building-fill.png" class="card-img-top" alt="Icon 2">
+                    <div class="card-body">
+                        <h5 class="card-title">Lembaga</h5>
+                    </div>
+                </button>
+            </div>
+            <div class="col-md-3">
+                <button class="card card-custom">
+                    <img src="/assets/tdesign_map-location.png" class="card-img-top" alt="Icon 3">
+                    <div class="card-body">
+                        <h5 class="card-title">Wilayah</h5>
+                    </div>
+                </button>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -22,4 +22,8 @@ Route::get('login',function () {
     return view('login');
 });
 
+Route::get('dashboard',function () {
+    return view('dashboard');
+});
+
 Route::get('/home', [ViewController::class, 'index']);
